939a8db8cf826badacf041d4a6e2aca91f848b04
TheOrbMaverick <enangita@gmail.com>
2024-05-20 01:24:31 +0100

    trying to add the front

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ This is the Capsoool business.
 This is going to be a real business and real app for storing electronic wills or as I like to call them time capsoools.
 
 It gives you the opportunity to save videos, images, voice and text messages that can be sent to your family after you pass.
+
+
+run the frontend folder with:
+npx expo start
+or
+npx expo start -c (to clear cache)
---
+++ frontend/images.d.ts
@@ -0,0 +1,24 @@
+declare module '*.png' {
+    const value: import('react-native').ImageSourcePropType;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.gif' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    const value: string;
+    export default value;
+  }
